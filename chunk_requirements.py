30caf5d0f5ce2a0e66ed027d58f9220281e01691
PIYUSH TIWARI <piyushayush822114@gmail.com>
2026-08-17 22:22:33 +0530

    just for practice

--- chunk_requirements.py
+++ chunk_requirements.py
@@ -1,3 +1,4 @@
+'''
 To Print:
 *****
 *   *
@@ -5,3 +6,12 @@ To Print:
 *   *
 *****
 using "Python"
+
+'''
+for i in range(1,6):
+  for j in range(1, i+1):
+    if i==1 or i==5 or j==1 or j==5:
+      print("*", end="")
+    else:
+      print(" ", end="")
+  print()
